220ebcf35d26196e06337ef2e16c9ed94d5e76f0
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-06-20 02:19:57 +0200

    add swagger and jwt

--- backend/book_store/urls.py
+++ backend/book_store/urls.py
@@ -1,21 +1,21 @@
-"""book_store URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    # apps
+    path('api-v1/',include('user.urls')),
+    path('api-v1/',include('product.urls')),
+    path('api-v1/',include('order.urls')),
+    # admin
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    # jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # swagger docs
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
