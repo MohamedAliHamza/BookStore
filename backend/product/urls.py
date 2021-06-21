13a5b5ec91c4bd5e49d03d6a49dd358dc7a60591
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-06-22 00:37:11 +0200

    Update product app

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+       path('books/', views.BookView.as_view()),
+       path('authors/', views.AuthorView.as_view()),
+       path('categories/', views.CategoryView.as_view()),
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,1 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializer import AuthorSerializer, CategorySerializer, BookSerializer
+from .models import Author, Category, Book
+from rest_framework.permissions import AllowAny
+
+class BookView(generics.ListAPIView):
+       queryset = Book.objects.all()
+       serializer_class = BookSerializer
+       permission_class =[]       
+
+class AuthorView(generics.ListAPIView):
+       queryset = Author.objects.all()
+       serializer_class = AuthorSerializer
+       permission_class =[]       
+
+class CategoryView(generics.ListAPIView):
+       queryset = Category.objects.all()
+       serializer_class = CategorySerializer
+       permission_class =[]       
