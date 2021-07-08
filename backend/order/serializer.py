ece078c9bcd5b50769cd22e145af9a79519a65ab
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-07-08 22:46:22 +0200

    feat: add order serializer

--- backend/order/serializer.py
+++ backend/order/serializer.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
-       order_id = serializers.IntegerField(read_only=True)
        class Meta:
               model = Order
               fields = ['id', 'client', 'mobile', 'address', 'paid', 'status', 'total_price']
