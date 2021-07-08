4fe9905a9588794e7f72cc274b070ba4ea4d5de4
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-07-08 04:07:35 +0200

    feat:add url for detail shopcart endpoint

--- backend/shopcart/urls.py
+++ backend/shopcart/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-       path('shopcart/', views.ShopCartView.as_view(), name='shopcart')       
-
+       path('shopcart/', views.ShopCartView.as_view(), name='shopcart'), 
+       path('shopcart/<int:pk>/', views.ShopCartDetailView.as_view(), name='shopcart-detail')
 ]
