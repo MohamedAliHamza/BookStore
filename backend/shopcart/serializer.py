a6511f74933c2694af09d3060a336833e7260ec0
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-07-08 04:07:04 +0200

    feat:add update cart

--- backend/shopcart/serializer.py
+++ backend/shopcart/serializer.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 from .models import ShopCart
 from product.models import Book
 from django.utils.translation import ugettext_lazy as _
+from rest_framework.reverse import reverse
 
 class ShopCartSerializer(serializers.Serializer):
     #book = serializers.SlugRelatedField(slug_field='name', read_only=True)
-    book_id = serializers.IntegerField(min_value=1, required=True)
-    quantity = serializers.IntegerField(required=True)
+    #id = serializers.IntegerField(read_only=True)
+    book_id = serializers.IntegerField(required=True)
+    quantity = serializers.IntegerField(min_value=1, required=True)
+    detail = serializers.SerializerMethodField()
 
     def create(self, validated_data):
         user =  self.context['request'].user
@@ -20,12 +23,20 @@ class ShopCartSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.quantity = validated_data.get('quantity', instance.quantity)
+        instance.save()
         return instance
 
     def validate(self, attrs):
         book_id = attrs.get('book_id')
         quantity = attrs.get('quantity')
         book = get_object_or_404(Book, id=book_id)
+        print(quantity)
         if quantity > book.stock_amount:
             raise serializers.ValidationError({"detail":_("This quantity is not in stock")})
         return attrs    
+    def get_detail(self, obj):
+        request = self.context['request']
+        detail = {
+            'link':reverse('shopcart-detail', kwargs={'pk':obj.id}, request=request)
+        } 
+        return detail
