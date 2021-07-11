d5e2be2ea1827a1f1af9f895bba0560de14c3214
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-07-11 02:04:53 +0200

    chore: update order validation

--- backend/order/models.py
+++ backend/order/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from user.models import User
 from django.utils.translation import ugettext_lazy as _
 
+# prodouct -> add prodouct to shopcart -> make order -> save shopcart as bought item -> clear shopcart 
+
 class Order(models.Model):
        PENDING = 'PENDING'
        IN_DELIVERY = 'In_delivery'
@@ -31,19 +33,3 @@ class Order(models.Model):
        @property
        def order_product(self):
               return list(self.client.shopcart_item.all())
-
-
-# class BoughtItem(models.Model):
-#     client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='BoughtItem_client', blank=True, null=True)
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_bought_items')
-#     product = models.ForeignKey(ProductBaseModel, on_delete=models.CASCADE,
-#                                 related_name='bought_items_product')
-#     quantity = models.IntegerField(default=1)
-#     total_price = models.FloatField()
-
-#     def __str__(self):
-#         return f"client: {self.client} product: {self.product} quantity: {self.quantity}"
-
-#     class Meta:
-#         verbose_name = _('BoughtItem')
-#         verbose_name_plural = _('BoughtItems')
