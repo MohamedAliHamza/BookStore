8a5aa88745d01d1360866f34cf3e1b41f1302f19
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-07-11 05:58:43 +0200

    feat:add order in admin panel

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Order
 
-admin.site.register(Order) 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','client', 'mobile', 'address', 'status', 'total_price', 'paid', 'ordered_date']
+    list_editable = ['status']
+    list_filter = ['ordered_date']
+    list_display_links = []
+    fields = []  
+    readonly_fields =['client', 'mobile', 'address', 'total_price', 'paid']
+
+    def has_delete_permission(self, request, obj=None):
+           return False
